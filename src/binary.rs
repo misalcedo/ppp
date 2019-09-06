1952c1e2fee9e1e31e78bf3fcd5ac456aa14cc08
Miguel Salcedo <msalcedo92@gmail.com>
2019-09-06 01:17:39 -0400

    Implement initial proxy protocol header v2.

--- src/binary.rs
+++ src/binary.rs
@@ -1,34 +1,56 @@
 use nom::IResult;
 use nom::bytes::complete::*;
+use nom::number::complete::*;
 use nom::combinator::*;
-use nom::character::complete::*;
 use nom::sequence::*;
-
-use nom::branch::alt;
-use nom::Err::*;
-use std::str::{FromStr, from_utf8};
-use std::net::IpAddr;
 
 extern crate test;
 
-const PREFIX: [u8; 12] = [0x0D , 0x0A , 0x0D , 0x0A , 0x00 , 0x0D , 0x0A , 0x51 , 0x55 , 0x49 , 0x54 , 0x0A];
+const PREFIX: &[u8] = b"\r\n\r\n\0\r\nQUIT\n";
 
+#[derive(Debug, Eq, PartialEq)]
 struct Header {
+    version_and_command: u8,
+    protocol_and_address_family: u8,
+    address_size: u16
+}
 
+impl Header {
+    fn new(version_and_command: u8, protocol_and_address_family: u8, address_size: u16) -> Header {
+        Header { version_and_command, protocol_and_address_family, address_size }
+    }
 }
 
 fn parse_header(input: &[u8]) -> IResult<&[u8], Header> {
-    map(tag(PREFIX), |_| Header {})(input)
+    map(
+        preceded(tag(PREFIX), tuple((be_u8, be_u8, be_u16))),
+        |(x, y, z)| Header::new(x, y, z)
+    )(input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
+    #[test]
+    fn no_address() {
+        let result = parse_header(b"\r\n\r\n\0\r\nQUIT\n\x03\x02\0\x01");
+
+        assert_eq!(result, Ok((&[][..], Header::new(0x3, 0x2, 1u16))));
+    }
+
     #[test]
     fn not_prefixed() {
-        let result = parse_header(&[0x0D , 0x0A , 0x0D , 0x0A , 0x01 , 0x0D , 0x0A , 0x51 , 0x55 , 0x49 , 0x54 , 0x0A]);
+        let result = parse_header(b"\r\n\r\n\x01\r\nQUIT\n");
 
-        assert_eq!(result.is_err(), true);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn incomplete() {
+        let bytes = [0x0D, 0x0A, 0x0D, 0x0A, 0x00];
+        let result = parse_header(&bytes[..]);
+
+        assert!(result.is_err());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ use ppp;
 
 fn main() {
     println!("{:?}", ppp::text::parse_header(b"PROXY UNKNOWN\r\n"));
+    println!("{:?}", unsafe { std::str::from_utf8_unchecked(&[0x0D, 0x0A, 0x0D, 0x0A, 0x00, 0x0D, 0x0A, 0x51, 0x55, 0x49, 0x54, 0x0A][..]) });
+    println!("{:?}", unsafe { std::str::from_utf8_unchecked(b"\r\n\r\n\0\r\nQUIT\n") });
 }
