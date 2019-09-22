0f59f2c8ebc2d8ee4f319aa12e871ceb79c08ee6
Miguel Salcedo <msalcedo92@gmail.com>
2019-09-22 00:02:49 -0400

    Cargo format

--- benches/binary.rs
+++ benches/binary.rs
@@ -1,38 +1,38 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
 use criterion::black_box;
+use criterion::Criterion;
 
 use ppp::parse_header;
 
 fn ipv6_input() -> Vec<u8> {
     let prefix = b"\r\n\r\n\0\r\nQUIT\n";
-    let mut input: Vec<u8> = Vec::with_capacity(prefix.len());	
+    let mut input: Vec<u8> = Vec::with_capacity(prefix.len());
 
-    input.extend_from_slice(prefix);	
+    input.extend_from_slice(prefix);
     input.push(0x21);
     input.push(0x21);
     input.extend(&[0, 45]);
-    input.extend(&[	
-        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,	
-        0xFF, 0xFF,	
-    ]);	
-    input.extend(&[	
-        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,	
-        0xFF, 0xF1,	
-    ]);	
-    input.extend(&[0, 80]);	
-    input.extend(&[1, 187]);	
-    input.extend(&[1, 0, 1, 5]);	
-    input.extend(&[2, 0, 2, 5, 5]);	
+    input.extend(&[
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF,
+    ]);
+    input.extend(&[
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xF1,
+    ]);
+    input.extend(&[0, 80]);
+    input.extend(&[1, 187]);
+    input.extend(&[1, 0, 1, 5]);
+    input.extend(&[2, 0, 2, 5, 5]);
 
     input
 }
 
 fn ipv4_input() -> Vec<u8> {
     let prefix = b"\r\n\r\n\0\r\nQUIT\n";
-    let mut input: Vec<u8> = Vec::with_capacity(prefix.len());	
+    let mut input: Vec<u8> = Vec::with_capacity(prefix.len());
 
     input.extend_from_slice(prefix);
     input.push(0x21);
@@ -49,13 +49,17 @@ fn ipv4_input() -> Vec<u8> {
     input
 }
 
-fn criterion_benchmark(c: &mut Criterion) {    
+fn criterion_benchmark(c: &mut Criterion) {
     let ipv6 = ipv6_input();
-    let ipv4 =ipv4_input();
+    let ipv4 = ipv4_input();
 
-    c.bench_function("ppp binary IPv6 without TLVs", |b| b.iter(|| parse_header(black_box(ipv6.as_slice()))));
+    c.bench_function("ppp binary IPv6 without TLVs", |b| {
+        b.iter(|| parse_header(black_box(ipv6.as_slice())))
+    });
 
-    c.bench_function("ppp binary IPv4 with TLVs", |b| b.iter(|| parse_header(black_box(ipv4.as_slice()))));
+    c.bench_function("ppp binary IPv4 with TLVs", |b| {
+        b.iter(|| parse_header(black_box(ipv4.as_slice())))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
--- benches/text.rs
+++ benches/text.rs
@@ -1,17 +1,30 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
 use criterion::black_box;
+use criterion::Criterion;
 
 use ppp::parse_header;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("ppp text tcp4", |b| b.iter(|| parse_header(black_box("PROXY TCP4 255.255.255.255 255.255.255.255 65535 65535\r\n".as_bytes()))));
-   
+    c.bench_function("ppp text tcp4", |b| {
+        b.iter(|| {
+            parse_header(black_box(
+                "PROXY TCP4 255.255.255.255 255.255.255.255 65535 65535\r\n".as_bytes(),
+            ))
+        })
+    });
+
     c.bench_function("ppp text tcp6", |b| b.iter(|| parse_header(black_box("PROXY TCP6 ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff 65535 65535\r\n".as_bytes()))));
 
-    c.bench_function("ppp text tcp6 compact", |b| b.iter(|| parse_header(black_box("PROXY TCP6 ffff::ffff ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff 65535 65535\r\n".as_bytes()))));
+    c.bench_function("ppp text tcp6 compact", |b| {
+        b.iter(|| {
+            parse_header(black_box(
+                "PROXY TCP6 ffff::ffff ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff 65535 65535\r\n"
+                    .as_bytes(),
+            ))
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
