bfddccd02187eeec240b0f67f483908138b80e7f
Miguel David Salcedo <misalcedo@github.com>
2021-12-21 16:49:16 +0000

    Revert "Cleanup examples."
    This reverts commit cb60371a68c09089909c6febce8a2e29311fdc5b.

--- examples/server.rs
+++ examples/server.rs
@@ -1,10 +1,46 @@
-use ppp::v1::BinaryParseError;
-use ppp::v2::{Header, ParseError};
 use ppp::{v1, v2, PartialResult};
 use std::io::{self, prelude::*};
 use std::net::{Ipv4Addr, SocketAddr, TcpListener, TcpStream};
 
 const RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
+
+fn handle_connection(mut client: TcpStream) -> io::Result<()> {
+    let mut buffer = [0; 512];
+    let mut read = client.read(&mut buffer)?;
+
+    let mut header = v2::Header::try_from(&buffer[..read]);
+
+    while header.is_incomplete() {
+        println!("Incomplete binary header. Read {} bytes so far.", read);
+
+        read += client.read(&mut buffer[read..])?;
+        header = v2::Header::try_from(&buffer[..read]);
+    }
+
+    match header {
+        Ok(header) => println!("Header: {}", header),
+        Err(error) => {
+            eprintln!("[ERROR] {:?} {}", buffer, error);
+
+            let mut header = v1::Header::try_from(&buffer[..read]);
+
+            while header.is_incomplete() {
+                println!("Incomplete text header. Read {} bytes so far.", read);
+
+                read += client.read(&mut buffer[read..])?;
+                header = v1::Header::try_from(&buffer[..read]);
+            }
+
+            match header {
+                Ok(header) => println!("Header: {}", header),
+                Err(error) => eprintln!("[ERROR] {:?} {}", buffer, error),
+            }
+        }
+    }
+
+    client.write_all(RESPONSE.as_bytes())?;
+    client.flush()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let address = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 7777);
@@ -19,62 +55,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
-}
-
-fn handle_connection(mut client: TcpStream) -> io::Result<()> {
-    let mut buffer = [0; 512];
-    let mut read = client.read(&mut buffer)?;
-
-    let header = read_v2_header(&mut client, &mut buffer, &mut read);
-
-    match header {
-        Ok(header) => println!("Header: {}", header),
-        Err(error) => {
-            eprintln!("[ERROR] {:?} {}", buffer, error);
-
-            let header = read_v1_header(&mut client, &mut buffer, &mut read);
-
-            match header {
-                Ok(header) => println!("Header: {}", header),
-                Err(error) => eprintln!("[ERROR] {:?} {}", buffer, error),
-            }
-        }
-    }
-
-    client.write_all(RESPONSE.as_bytes())?;
-    client.flush()
-}
-
-fn read_v1_header(
-    client: &mut TcpStream,
-    mut buffer: &mut [u8; 512],
-    mut read: &mut usize,
-) -> Result<Header, BinaryParseError> {
-    let mut header = v1::Header::try_from(&buffer[..read]);
-
-    while header.is_incomplete() {
-        println!("Incomplete text header. Read {} bytes so far.", read);
-
-        read += client.read(&mut buffer[read..])?;
-        header = v1::Header::try_from(&buffer[..read]);
-    }
-
-    header
-}
-
-fn read_v2_header(
-    client: &mut TcpStream,
-    mut buffer: &mut [u8; 512],
-    mut read: &mut usize,
-) -> Result<Header, ParseError> {
-    let mut header = v2::Header::try_from(&buffer[..read]);
-
-    while header.is_incomplete() {
-        println!("Incomplete binary header. Read {} bytes so far.", read);
-
-        read += client.read(&mut buffer[read..])?;
-        header = v2::Header::try_from(&buffer[..read]);
-    }
-
-    header
 }
