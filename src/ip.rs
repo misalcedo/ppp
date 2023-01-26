46540f20f50772e52e912f2267885d6613f87abc
Miguel D. Salcedo <misalcedo@github.com>
2023-01-26 10:02:07 -0500

    Use socket address instead of our own version.

--- src/ip.rs
+++ src/ip.rs
@@ -1,54 +1,43 @@
 //! Models for storing IP v4 and v6 addresses and ports.
 
-use std::net::{Ipv4Addr, Ipv6Addr};
+use std::net::{SocketAddrV4, SocketAddrV6};
 
 /// The source and destination IPv4 addresses and TCP ports of a header.
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct IPv4 {
-    pub source_address: Ipv4Addr,
-    pub source_port: u16,
-    pub destination_address: Ipv4Addr,
-    pub destination_port: u16,
+    pub source: SocketAddrV4,
+    pub destination: SocketAddrV4,
 }
 
 impl IPv4 {
     /// Create a new IPv4 addresses.
-    pub fn new<T: Into<Ipv4Addr>>(
-        source_address: T,
-        destination_address: T,
-        source_port: u16,
-        destination_port: u16,
+    pub fn new<T: Into<SocketAddrV4>>(
+        source: T,
+        destination: T,
     ) -> Self {
         IPv4 {
-            source_address: source_address.into(),
-            source_port,
-            destination_address: destination_address.into(),
-            destination_port,
+            source: source.into(),
+            destination: destination.into(),
         }
     }
 }
+
 /// The source and destination IPv6 addresses and TCP ports of a header.
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct IPv6 {
-    pub source_address: Ipv6Addr,
-    pub source_port: u16,
-    pub destination_address: Ipv6Addr,
-    pub destination_port: u16,
+    pub source: SocketAddrV6,
+    pub destination: SocketAddrV6,
 }
 
 impl IPv6 {
     /// Create a new IPv6 addresses.
-    pub fn new<T: Into<Ipv6Addr>>(
-        source_address: T,
-        destination_address: T,
-        source_port: u16,
-        destination_port: u16,
+    pub fn new<T: Into<SocketAddrV6>>(
+        source: T,
+        destination: T,
     ) -> Self {
         IPv6 {
-            source_address: source_address.into(),
-            source_port,
-            destination_address: destination_address.into(),
-            destination_port,
+            source: source.into(),
+            destination: destination.into(),
         }
     }
 }
